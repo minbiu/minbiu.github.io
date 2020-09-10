fd9afcdb62675f9ded4f07b7ed3997dcd1b7fba6
minbiu <446842237@qq.com>
2020-09-11 07:37:41 +0800

    Site updated: 2020-09-11 07:37:37

---
+++ dist/music.js
@@ -0,0 +1,27 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    volume: 0.7,
+audio: [
+      {
+        name: "風の詩",
+        artist: '押尾コータロー',
+        url: 'https://od.lk/s/NDRfMTIyNTc1ODNf/001.mp3',
+        cover: 'http://p1.music.126.net/w4mS_DbgBlJLzzkiuQcIBA==/109951163405911243.jpg?param=130y130.jpg',
+      },
+      {
+        name: 'Canon In D Major',
+        artist: '纯音乐',
+        url: 'https://od.lk/s/NDRfMTIyNTc2MThf/003.mp3',
+        cover: 'http://p1.music.126.net/w4mS_DbgBlJLzzkiuQcIBA==/109951163405911243.jpg?param=130y130.jpg',
+      },
+      {
+
+        name: 'lovin‘ you',
+        artist: '押尾コータロー',
+        url: 'http://fs.open.kugou.com/a2efcb258c58405cde0d656422d98c72/5d3e8e98/G107/M02/08/07/S5QEAFvcAq6Af8ZvADzUmYcpswA651.mp3',
+        cover: 'http://p1.music.126.net/w4mS_DbgBlJLzzkiuQcIBA==/109951163405911243.jpg?param=130y130.jpg',
+      }
+   ]
+});
