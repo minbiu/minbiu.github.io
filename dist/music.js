c5690d64b2e851d5f931096f1ce89f38d565c019
minbiu <630328932@qq.com>
2025-07-12 21:16:00 +0800

    Site updated: 2025-07-12 21:16:00

--- dist/music.js
+++ dist/music.js
@@ -7,21 +7,21 @@ audio: [
       {
         name: "風の詩",
         artist: '押尾コータロー',
-        url: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/風の詩(风之诗).mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/風の詩(风之诗).jpg',
+        url: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/風の詩(风之诗)001.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/風の詩(风之诗)001.jpg',
       },
       {
         name: 'Canon In D Major',
         artist: '纯音乐',
-        url: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/Canon in D major.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/Canon in D major.jpg',
+        url: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/Canon in D major002.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/Canon in D major002.jpg',
       },
       {
 
         name: 'lovin‘ you',
         artist: '押尾コータロー',
-        url: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/LOVIN' YOU.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/LOVIN' YOU.jpg',
+        url: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/LOVIN' YOU003.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/minbiu/bgmusic/LOVIN' YOU003.jpg',
       }
    ]
 });
